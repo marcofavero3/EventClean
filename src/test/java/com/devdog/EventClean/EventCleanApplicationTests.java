754a9c3a03a97491c84b34dfd6594839a9ecf43b
marcofavero3 <marcofavero3@live.com>
2026-03-23 10:29:07 -0300

    primeiro commit - estrutura inicial Spring Boot

---
+++ src/test/java/com/devdog/EventClean/EventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdog.EventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
